53198395576d6ac7b62136036b6b1e7a63fc40cb
Patrick Ryan <pmr3mh@gmail.com>
2025-01-01 12:21:28 -0700

    test retool

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,7 @@
 import { redirect } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
-export const load = (async () => {
-  throw redirect(307, "/reading-list");
-}) satisfies PageLoad;
+// export const load = (async () => {
+// throw redirect(307, "/reading-list");
+// }) satisfies PageLoad;
+//
